5bbd6fb56452509429776f54e8876c3be2cda243
huangming98 <huangming98@163.com>
2022-09-13 21:21:48 +0800

    Site updated: 2022-09-13 21:21:48

--- js/switch_language.js
+++ js/switch_language.js
@@ -1,7 +1,7 @@
 var _prevOnload = window.onload;
 
 window.onload = function() {
-    var switchLang = document.getElementsByClassName("menu-item menu-item-switch-to-chinese")[0];
+    var switchLang = document.getElementsByClassName("menu-item menu-item-switch-to-english")[0];
     switchLang.onclick = function() {
         var href = window.location.href;
         var includesKeywords = href.includes("/cn/homepage/") || href.includes("/cn/home/")|| href.includes("/cn/publications/") || href.includes("/cn/resources/")|| href.includes("/cn/link/")|| href.includes("/cn/archives/");
